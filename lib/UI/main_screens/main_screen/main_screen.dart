043e3ae8e71faf5e1757911e033164ad0aa5da29
Mahmoud Ali <mahmoud.aali20@gmail.com>
2021-06-29 15:52:11 +0200

    home screen

--- lib/UI/main_screens/main_screen/main_screen.dart
+++ lib/UI/main_screens/main_screen/main_screen.dart
@@ -13,83 +13,189 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Scaffold(
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(color: MColors.covidMain),
-              child: SafeArea(
-                  child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: 65.h,
-                  width: 100.w,
-                  decoration: BoxDecoration(
-                      color: MColors.covidThird,
-                      borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(25),
-                          topLeft: Radius.circular(25))),
-                ),
-              )),
-            ),
-            // Positioned(
-            //   right: 0,
-            //   top: 0,
-            //   child: Container(
-            //       height: 15.h,
-            //       width: 15.h,
-            //       child: Positioned(
-            //         top: 0,
-            //         child: CircleAvatar(
-            //           maxRadius: 10,
-            //           minRadius: 10,
-            //           backgroundColor: Colors.white,
-            //           child: Image.asset(
-            //             "assets/images/logo-trial.png",
-            //             fit: BoxFit.cover,
-            //           ),
-            //         ),
-            //       )),
-            // ),
-            SafeArea(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(color: MColors.covidMain)),
+          buildTotalCases(),
+          buildVirusIcon(),
+          SafeArea(
+              child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 70.h,
+              width: 100.w,
+              decoration: BoxDecoration(
+                  color: MColors.covidThird,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(5.h),
+                      topLeft: Radius.circular(5.h))),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: 10.h,
-                      width: 10.h,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 7.w, vertical: 7.h),
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              // color: Colors.amber,
-                              color: Colors.grey[200],
-                              spreadRadius: .5,
-                              blurRadius: 2,
-                              offset: Offset(0, 1))
-                        ],
-                        color: Color(0xFFF3F4F5),
-                        borderRadius: BorderRadius.circular(20),
-                        // border: Border.all()
-                      ),
-                    ),
+                  Spacer(),
+                  buildCovidCheckContainer(),
+                  Spacer(),
+                  // SizedBox(height: 5.h),
+                  buildHomeIcons(),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Spacer(),
+                      HomeItem(
+                          icon: Icon(FontAwesomeIcons.mapMarkedAlt,
+                              color: MColors.covidMain),
+                          text: "Map"),
+                      Spacer(),
+                      HomeItem(
+                          icon: Icon(FontAwesomeIcons.info,
+                              color: MColors.covidMain),
+                          text: "Information"),
+                      Spacer(),
+                    ],
                   ),
+                  Spacer(),
                 ],
               ),
             ),
-          ],
-        ),
+          )),
+          // Positioned(
+          //   right: 0,
+          //   top: 0,
+          //   child: Container(
+          //       height: 15.h,
+          //       width: 15.h,
+          //       child: Positioned(
+          //         top: 0,
+          //         child: CircleAvatar(
+          //           maxRadius: 10,
+          //           minRadius: 10,
+          //           backgroundColor: Colors.white,
+          //           child: Image.asset(
+          //             "assets/images/logo-trial.png",
+          //             fit: BoxFit.cover,
+          //           ),
+          //         ),
+          //       )),
+          // ),
+        ],
       ),
       bottomNavigationBar: buildBottomNavigationBar(),
     );
   }
 
+  Positioned buildVirusIcon() {
+    return Positioned(
+        top: 12.h,
+        left: -10.w,
+        child: Opacity(
+          opacity: .8,
+          child: Image(
+              image: AssetImage("assets/images/virus.png"),
+              height: 20.h,
+              width: 20.h),
+        ));
+  }
+
+  Positioned buildTotalCases() {
+    return Positioned(
+      top: 10.h,
+      right: 20.w,
+      child: Container(
+        height: 10.h,
+        width: 30.w,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("6738",
+                style: TextStyle(
+                    fontFamily: "Plex",
+                    fontWeight: FontWeight.bold,
+                    color: MColors.covidThird,
+                    fontSize: 30.sp)),
+            Text("Total Cases",
+                style: TextStyle(
+                    fontFamily: "Plex",
+                    // fontWeight: FontWeight.bold,
+                    color: MColors.covidThird,
+                    fontSize: 12.sp)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildHomeIcons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Spacer(),
+        HomeItem(
+            icon: Icon(FontAwesomeIcons.mapMarkedAlt, color: MColors.covidMain),
+            text: "Map"),
+        Spacer(),
+        HomeItem(
+            icon: Icon(FontAwesomeIcons.info, color: MColors.covidMain),
+            text: "Information"),
+        Spacer(),
+        HomeItem(
+            icon: Icon(FontAwesomeIcons.mapMarkedAlt, color: MColors.covidMain),
+            text: "Map"),
+        Spacer()
+      ],
+    );
+  }
+
+  Container buildCovidCheckContainer() {
+    return Container(
+      height: 18.h,
+      width: 90.w,
+      padding: EdgeInsets.only(left: 2.w, right: 2.h, top: 2.h),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+              // color: Colors.amber,
+              color: Colors.grey[200],
+              spreadRadius: 1,
+              blurRadius: 2,
+              offset: Offset(0, 5))
+        ],
+        color: Color(0xFFF3F4F5),
+        borderRadius: BorderRadius.circular(3.h),
+
+        // border: Border.all()
+      ),
+      child: Row(
+        children: [
+          Image(image: AssetImage("assets/images/doctor.png")),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Covid Check",
+                  style: TextStyle(
+                      fontFamily: "Plex",
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.sp)),
+              Text("Based on common\nsyndrome",
+                  style: TextStyle(fontFamily: "Plex", fontSize: 10.sp)),
+            ],
+          ),
+          Spacer(),
+          Icon(Icons.arrow_forward_ios, color: MColors.kTextColor, size: 2.h),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+
   BottomNavigationBar buildBottomNavigationBar() {
     return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
       items: [
         BottomNavigationBarItem(
           icon: Icon(FontAwesomeIcons.home),
@@ -103,6 +209,46 @@ class _MainScreenState extends State<MainScreen> {
             icon: Icon(FontAwesomeIcons.userAlt), label: "Profile"),
       ],
       selectedItemColor: MColors.covidMain,
+    );
+  }
+}
+
+class HomeItem extends StatelessWidget {
+  const HomeItem({
+    this.icon,
+    this.text,
+    Key key,
+  }) : super(key: key);
+  final String text;
+  final Widget icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 12.h,
+          width: 12.h,
+          padding: EdgeInsets.symmetric(horizontal: 2.w, vertical: 2.h),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  // color: Colors.amber,
+                  color: Colors.grey[200],
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 5))
+            ],
+            color: Color(0xFFF3F4F5),
+            borderRadius: BorderRadius.circular(3.h),
+
+            // border: Border.all()
+          ),
+          child: icon,
+        ),
+        SizedBox(height: 1.5.h),
+        Text(text, style: TextStyle(fontFamily: "Plex"))
+      ],
     );
   }
 }
